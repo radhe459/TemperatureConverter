f7c24c98cde31ae6156861305b3dbf6f1be87e60
Radhe Shyam Chaubey <informativersc1234@gmail.com>
2024-06-19 22:25:04 +0530

    added degree notation

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ const fahrenheitToCelcius = document.getElementById("cRadio")
 
 
 function celciusToFahrenheitConverter(temperature){
-    return (temperature*(9/5))+32
+    return (temperature*(9/5))+32 + "°F"
 }
 
 function fahrenheitToCelciusConverter(temperature){
-    return ((5/9)*(temperature-32))
+    return ((5/9)*(temperature-32)) + "°C"
 }
 
 document.getElementById("Submit").onclick = function(){
     let input = document.getElementById("temperature").value
-    console.log(typeof input)
     input = Number(input)
     document.getElementById("result").textContent = celciusToFahrenheit.checked ? celciusToFahrenheitConverter(input) : fahrenheitToCelcius.checked ? fahrenheitToCelciusConverter(input) : "Please select any option";
 }
